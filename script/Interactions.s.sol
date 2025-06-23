5b92031906ff74971bbcad03d61ddc5b21fc6ecb
ykgneh <chewhengky9@gmail.com>
2025-06-23 16:59:05 +0700

    uWu

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
